614c622a1003a746d8a2f4b72cbd6223b322da53
Bayzid Alim <shsajib@Bayzids-MacBook-Air.local>
2025-01-18 14:41:19 +0600

    committed

---
+++ src/Components/UserDetails/UserDetails.jsx
@@ -0,0 +1,11 @@
+
+
+const UserDetails = () => {
+    return (
+        <div>
+            <h2>Details about user</h2>
+        </div>
+    );
+};
+
+export default UserDetails;
---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Home from './Components/Home/Home.jsx'
+import App from './App.jsx'
+import { createBrowserRouter,RouterProvider, } from 'react-router-dom'
+import About from './Components/About/About.jsx'
+import Contact from './Components/Contact/Contact.jsx'
+import Users from './Components/Users/Users.jsx'
+import UserDetails from './Components/UserDetails/UserDetails.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    children:[
+      {
+        path: '/about',
+        element: <About></About>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      },
+      {
+        path: '/users',
+        loader: ()=>fetch('https://jsonplaceholder.typicode.com/users'),
+        element: <Users></Users>
+      },
+      {
+        path: '/user/:userID',
+        loader: ({params})=> console.log(params),
+        element: <UserDetails></UserDetails>
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
